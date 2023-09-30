2d9145baa60f7c0f1e08ebe1a049ee733d1ca308
InTheBloom <nato.rider.smm2@gmail.com>
2023-09-30 22:35:45 +0900

    fix

--- build.d
+++ build.d
@@ -1,5 +1,8 @@
 import std.file;
 import core.stdc.stdlib;
+import std.stdio;
+import std.string : chomp;
+import std.format;
 
 void main () {
     string Separator;
@@ -9,8 +12,21 @@ void main () {
         Separator = "/";
     }
 
-    if (exists(getcwd()~Separator~"docs")) {
+    bool doDelete = checkDeleteOrNot();
+
+    if (exists(getcwd()~Separator~"docs") && doDelete) {
         rmdirRecurse(getcwd()~Separator~"docs");
+        writeln(format("%s is cleared.", getcwd()~Separator~"docs"));
     }
     system("hugo");
+}
+
+bool checkDeleteOrNot () {
+    writeln("Do you want to clear the previous directory before building the site? [Y/N]");
+    while (true) {
+        auto reply = readln.chomp;
+        if (reply == "Y") return true;
+        if (reply == "N") return false;
+        writeln("Please follow the specified format.");
+    }
 }
